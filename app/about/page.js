32f483ab433ca138c2d832cbf871babe4cc6324e
CojaONIX <cojaonix@gmail.com>
2024-06-04 22:14:25 +0200

    20.2 next/image

--- app/about/page.js
+++ app/about/page.js
@@ -1,8 +1,16 @@
+import Image from "next/image";
 
 export default function About() {
     return (
         <>
             <h1>About Page</h1>
+            <Image
+                src="/images/16.jpg"
+                alt="Ferrari Image"
+                width="256"
+                height="192"
+            />
+
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed odio dui. Curabitur pellentesque neque et libero sodales, sit amet accumsan ligula tincidunt. Aliquam auctor justo eu enim interdum, nec vulputate erat porta. Mauris vel enim a odio ullamcorper dapibus id at massa. Donec non mi a sem scelerisque tristique sit amet eu ex. Praesent sed dui a velit scelerisque lacinia. Curabitur placerat dolor in pretium eleifend. Etiam eget magna nulla. Sed vitae magna in justo varius suscipit. Maecenas elementum feugiat massa, sed feugiat ante lobortis feugiat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed viverra faucibus mattis. Duis sit amet nisi eu nisi venenatis porta at vitae enim. Sed tempus maximus tellus non scelerisque. Nulla maximus tortor eget scelerisque mollis.</p>
         </>
     )
