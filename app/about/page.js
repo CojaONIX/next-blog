da8faea5e7b8287dae030589ac817f3dccb1464c
CojaONIX <cojaonix@gmail.com>
2024-06-04 22:37:09 +0200

    20.4 fetching

--- app/about/page.js
+++ app/about/page.js
@@ -1,6 +1,11 @@
 import Image from "next/image";
 
-export default function About() {
+export default async function About() {
+
+    const response = await fetch("https://dummyjson.com/posts?limit=1");
+    const data = await response.json();
+    const post = data['posts'][0];
+
     return (
         <>
             <h1>About Page</h1>
@@ -11,7 +16,8 @@ export default function About() {
                 height="192"
             />
 
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed odio dui. Curabitur pellentesque neque et libero sodales, sit amet accumsan ligula tincidunt. Aliquam auctor justo eu enim interdum, nec vulputate erat porta. Mauris vel enim a odio ullamcorper dapibus id at massa. Donec non mi a sem scelerisque tristique sit amet eu ex. Praesent sed dui a velit scelerisque lacinia. Curabitur placerat dolor in pretium eleifend. Etiam eget magna nulla. Sed vitae magna in justo varius suscipit. Maecenas elementum feugiat massa, sed feugiat ante lobortis feugiat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed viverra faucibus mattis. Duis sit amet nisi eu nisi venenatis porta at vitae enim. Sed tempus maximus tellus non scelerisque. Nulla maximus tortor eget scelerisque mollis.</p>
+            <h4>{post.title}.</h4>
+            <p>{post.body}</p>
         </>
     )
 }
