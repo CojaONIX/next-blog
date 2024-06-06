399ee4108f2aa5698fecefcbb37365b4727b6077
CojaONIX <cojaonix@gmail.com>
2024-06-06 13:28:17 +0200

    22.17 auth_user

---
+++ pages/api/auth_user.js
@@ -0,0 +1,16 @@
+
+export default async function handler(req, res) {
+    fetch('https://dummyjson.com/auth/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            username: 'emilys',
+            password: 'emilyspass',
+            expiresInMins: 30, // optional, defaults to 60
+        })
+    })
+        .then(res => res.json())
+        .then(console.log);
+
+    res.status(200).json({message: 'Successfully auth'});
+}
